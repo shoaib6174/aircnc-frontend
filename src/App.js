86026b577f2ad57fdfcf7e050489e5e071e647a6
shoaib6174 <shoaib6174@gmail.com>
2020-06-07 00:38:59 +0600

    done

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import Navbar from './Components/Navbar/Navbar';
 function App() {
   return (
     <div className="App">
-        <Navbar/>
+        
     
           <Switch>
             <Route exact path="/" component={Homepage} />
--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter as Router} from "react-router-dom";
+import Navbar from './Components/Navbar/Navbar';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Navbar/>
     <Router>
       <App />
     </Router>
